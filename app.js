c1061b46db747b1f29d4211b17227c24d5a4177a
rafliyulistiawan <rafliyulistiawan@gmail>
2024-02-26 00:45:04 +0700

    add gitignore, fix template string, fix package lock

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log('Server listening on port ${PORT}...');
+  console.log(`Server listening on port ${PORT}...`);
 });
