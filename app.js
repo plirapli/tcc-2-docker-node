b067830abf81502a0b76762663dca75bae95aa29
rafliyulistiawan <rafliyulistiawan@gmail>
2024-02-26 00:43:39 +0700

    fix port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send('Hello TCC-A 🦉!');
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Server listening on port ${PORT}...');
 });
